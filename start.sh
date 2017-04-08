ec8991738b06a9f0033c4b04cffbf771d9a2727d
Joel Burton <joel@joelburton.com>
2017-04-08 09:20:46 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -o errexit
+set -o errexit    # stop script if any error happens
 
-if [ "$#" -ne 1 ];
+if [ "$#" -ne 1 ];          # make sure user gives a port #
     then echo "error -- usage: bash start.sh port-number";
     exit 1;
 fi
@@ -15,7 +15,6 @@ gunicorn \
     --log-file error.log \
     --access-logfile access.log \
     --pid server.pid \
-    -n $(basename $(pwd)) \
     server:app
 
 echo -e "\nStarted on port $1.\n"
@@ -31,10 +30,5 @@ echo -e "\nStarted on port $1.\n"
 # --log-file : log other Flask output (errors, etc) here
 
 # --pid : save the process ID of gunicorn here (we use this in stop.sh)
-
-# -n : set the process title (as shown in `ps x`) to name of this app
-#         (this isn't critical, but is helpful if you have several apps
-#         running under gunicorn -- it makes it easy to see that this
-#         instance of gunicorn is running this app)
 
 # server:app : use a Flask/WSGI app called "app" in Python module "server"
